ddd7b85a0a608c6aea047a3dfb530598da7a1a56
Francisco José Barba López <franciscojose.barbalopez@gmail.com>
2025-03-14 18:56:37 +0100

    feat: created install command
    created install command with the possibility of installing playwright on it before publishing assets

---
+++ src/Console/Commands/Install.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace FranBarbaLopez\LaravelPlaywright\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Process;
+
+class Install extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'laravel-playwright:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install all of the Laravel Playwright resources';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(private Filesystem $files)
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle(): void
+    {
+        if (! $this->isPlaywrightInstalled()) {
+            if ($this->confirm('Playwright is not installed. Would you like to install it?', false)) {
+                $packageManager = $this->choice('Which package manager would you like to use?', ['npm', 'yarn', 'pnpm'], 0);
+            
+                $this->comment('Installing Playwright...');
+    
+                match ($packageManager) {
+                    'npm' => Process::tty()->run('npm init playwright@latest'),
+                    'yarn' => Process::tty()->run('yarn create playwright'),
+                    'pnpm' => Process::tty()->run('pnpm create playwright'),
+                };
+            } else {
+                $this->error('Playwright is required to install Laravel Playwright.');
+    
+                return;
+            }
+        }
+
+        $this->comment('Publishing Laravel Playwright Assets...');
+        $this->callSilently('vendor:publish', ['--tag' => 'laravel-playwright-assets']);
+
+        $this->info('Laravel Playwright scaffolding installed successfully.');
+    }
+
+    /**
+     * Determine if Playwright is installed.
+     * 
+     * @return bool
+     */
+    private function isPlaywrightInstalled(): bool
+    {
+        $packageJson = json_decode($this->files->get(base_path('package.json')), true);
+
+        return Arr::get($packageJson, 'devDependencies.@playwright/test') || Arr::get($packageJson, 'dependencies.@playwright/test');
+    }
+}
