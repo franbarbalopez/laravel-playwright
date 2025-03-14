a891838a3d1ae2292639596720f52a7e4d9ce101
franbarbalopez <franciscojose.barbalopez@gmail.com>
2025-03-14 18:13:09 +0100

    wip

--- src/Console/Commands/Install.php
+++ src/Console/Commands/Install.php
@@ -3,6 +3,7 @@
 namespace FranBarbaLopez\LaravelPlaywright\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
 
 class Install extends Command
 {
@@ -28,18 +29,43 @@ class Install extends Command
     public function handle(): void
     {
         if (! $this->isPlaywrightInstalled()) {
-            return;
+            $this->proposePlaywrightInstallation();
         }
 
         $this->comment('Publishing Laravel Playwright Assets...');
-        $this->callSilent('vendor:publish', ['--tag' => 'laravel-playwright-assets']);
+        $this->callSilently('vendor:publish', ['--tag' => 'laravel-playwright-assets']);
 
         $this->info('Laravel Playwright scaffolding installed successfully.');
     }
 
-    private function isPlaywrightInstalled()
+    /**
+     * Determine if Playwright is installed.
+     * 
+     * @return bool
+     */
+    private function isPlaywrightInstalled(): bool
     {
-        dd(base_path());
-        return file_exists(base_path('package.json'));
+        $packageJson = json_decode(base_path('package.json'), true);
+
+        return Arr::get($packageJson, 'devDependencies.@playwright/test') || Arr::get($packageJson, 'dependencies.@playwright/test');
+    }
+
+    private function proposePlaywrightInstallation(): void
+    {
+        if ($this->confirm('Playwright is not installed. Would you like to install it?', false)) {
+            $packageManager = $this->choice('Which package manager would you like to use?', ['npm', 'yarn', 'pnpm'], 0);
+        
+            $this->comment('Installing Playwright...');
+
+            match ($packageManager) {
+                'npm' => $this->callSilently('npm init playwright@latest'),
+                'yarn' => $this->callSilently('yarn create playwright'),
+                'pnpm' => $this->callSilently('pnpm create playwright'),
+            };
+        } else {
+            $this->warn('Playwright is required to install Laravel Playwright.');
+
+            return;
+        }
     }
 }
